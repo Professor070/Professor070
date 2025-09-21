bbeb9d988638d6e9cb00a0fda2b070c7630051f2
Professor070 <us058771@gmail.com>
2025-09-21 11:44:47 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Rename this file to config.py and fill in your credentials
-
-API_ID = 29388606  # Your API ID (as integer, not string)
-API_HASH = "ddc1032e4e1fd0216362d18b68afd848"
-BOT_TOKEN = "7777058850:AAGnVz_8MKsiUij35N-fupSeoCMgQb4Hx2g"
